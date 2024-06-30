88461d6c56f9959282931506feda882831104f18
indigorose <indigorose049@gmail.com>
2024-06-30 22:07:26 +0100

    Add base html and css

--- social_links_profile/src/App.jsx
+++ social_links_profile/src/App.jsx
@@ -8,7 +8,24 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1>Hello</h1>
+        <section className="card">
+          
+          <div className="card--about">
+            <img src="" alt="" />
+            <h1 className="card--name">
+              Jessica Randall
+            </h1>
+            <h2 className="card--sub">London, United Kingdom</h2>
+            <p className="card--about">"Front-end developer and avid reader"</p>
+          </div>
+          <ul className="card--list">
+            <li className="card--item"><a href="_blank" className="card--link">GitHub</a></li>
+            <li className="card--item"><a href="_blank" className="card--link">Frontend Mentor</a></li>
+            <li className="card--item"><a href="_blank" className="card--link">Linkedin</a></li>
+            <li className="card--item"><a href="_blank" className="card--link">Twitter</a></li>
+            <li className="card--item"><a href="_blank" className="card--link">Instagram</a></li>
+          </ul>
+        </section>
       </div>
 
     </>
