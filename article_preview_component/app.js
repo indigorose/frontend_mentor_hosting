95e847d9bbb311b133ebb76774632d21ad98070e
indigorose <indigorose049@gmail.com>
2024-11-11 21:48:10 +0000

    Update: html refactoring for mobile and desktop

--- article_preview_component/app.js
+++ article_preview_component/app.js
@@ -7,3 +7,16 @@ function toggleDesktopShare() {
 	const shareboxDesktop = document.querySelector('.sharebox__desktop');
 	shareboxDesktop.classList.toggle('is-visible');
 }
+
+// Mobile share bar interaction
+
+const profileContainer = document.querySelector('.profile__container');
+
+const shareboxMobile = document.querySelector('.share-button');
+
+const shareboxMobileBtn = document.querySelector('.share__button-mobile');
+
+shareboxMobileBtn.addEventListener('click', function () {
+	profileContainer.style.display = 'none';
+	shareboxMobile.style.display;
+});
