f9a534f10debcc8d737477d50d85d28855fffb94
indigorose <indigorose049@gmail.com>
2024-11-13 21:29:38 +0000

    Update: complete mobile styling and javascript interaction

--- article_preview_component/app.js
+++ article_preview_component/app.js
@@ -10,20 +10,22 @@ function toggleDesktopShare() {
 
 // Mobile share bar interaction
 
-const profileContainer = document.querySelector('.profile__container-mobile');
+const profileContainerMobile = document.querySelector(
+	'.profile__container-mobile'
+);
 
 const shareboxMobile = document.querySelector('.sharebox__mobile');
 
-const shareboxBtn = document.querySelector('.sharebox__button-mobile');
+const shareboxBtn = document.querySelector('.share__button-mobile');
 
-const shareboxMobileBtn = document.querySelector('.mobile__btn');
+const shareboxMobileBtn = document.querySelector('.sharebox__button-mobile');
 
 shareboxBtn.addEventListener('click', function () {
-	profileContainer.style.display = 'none';
-	shareboxMobile.style.display = 'flex';
+	profileContainerMobile.style.display = 'none';
+	shareboxMobile.style.display = 'grid';
 });
 
 shareboxMobileBtn.addEventListener('click', function () {
-	profileContainer.style.display = 'flex';
+	profileContainerMobile.style.display = 'grid';
 	shareboxMobile.style.display = 'none';
 });
