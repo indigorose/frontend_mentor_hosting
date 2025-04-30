239a6a8a4bab67a56b7248ed48e6a1d1337fe3b9
indigorose <indigorose049@gmail.com>
2025-04-30 22:19:21 +0100

    Enhance tip button structure and styling for better responsiveness

---
+++ tip-calculator-app/script.js
@@ -0,0 +1,4 @@
+// How to make the calculator responsive
+const billInput = document.getElementById('bill');
+const customTipInput = document.getElementById('custom');
+const peopleInput = document.getElementById('people');
