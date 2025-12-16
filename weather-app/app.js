58e606c7eeb0737b422a7d7a42a1ae0257512dde
indigorose <indigorose049@gmail.com>
2025-12-16 21:12:21 +0000

    Update api call testing

---
+++ weather-app/app.js
@@ -0,0 +1,21 @@
+// Replace with the coordinates you want
+// const latitude = 51.5074; // e.g., London
+// const longitude = -0.1278;
+
+// const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+
+// fetch(url)
+// 	.then((response) => {
+// 		if (!response.ok) {
+// 			throw new Error(
+// 				'Network response was not ok ' + response.statusText
+// 			);
+// 		}
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log('Weather data:', data);
+// 	})
+// 	.catch((error) => {
+// 		console.error('Fetch error:', error);
+// 	});
